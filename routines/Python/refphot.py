fdbb58cb56146a96cb7fcc78c336ee9d0a0db6fe
ryanoelkers <ryanjamesoelkers@gmail.com>
2018-03-06 11:54:38 -0600

    Update refphot.py

--- routines/Python/refphot.py
+++ routines/Python/refphot.py
@@ -1,7 +1,7 @@
 #this program will do the photometry on the reference frame to get stars 
 #for the subtraction and to get stars for later
 
-#if you use this code, please cite Oelkers et al. 2015, AJ, 149, 50
+#if you use this code, please cite Oelkers & Stassun 2018
 
 #import the relevant libraries for basic tools
 import numpy
@@ -16,82 +16,124 @@ from photutils import DAOStarFinder
 from photutils import aperture_photometry
 from photutils import CircularAperture
 from photutils import CircularAnnulus
-import pyfits
 
 #for reading in fits files
 from astropy.io import fits
+from astropy.wcs import WCS
 
 #import relevant libraries for a list
 import glob, os
 from os import listdir
 from os.path import isfile, join, exists
 
-#import relevant spline libraries
-from scipy.interpolate import Rbf
-
 ###BEGIN UPDATE#####
-#what is the exposure time?
-exp_time = 60.0
-
-#optimal radius
-rad = 3.0
+camera = '2'
+ccd = '2'
 
 #useful directories
-cdedir = '../Python/' #code directory
-caldir = '../calib/' # directory to output the flux files and location of master frame
-
+cdedir = '../code/master/' #code directory
+caldir = '../code/master/fin/' # directory to output the flux files and location of master frame
+clndir = '../clean/'#directory with the cleaned images
 ##END UPDATE####
 
 #read in the master frame
-mlist = fits.open(caldir+'master.fits')
-mheader = mlist[0].header #get the header info
-mast = mlist[0].data #get the image info
-exp_time = pyfits.getval(caldir+'master.fits','EXPTIME')
+mast, mheader = fits.getdata(caldir+camera+'_'+ccd+'_master.fits', header = True)
 
-#get the positions in the master
-mean, median, std = sigma_clipped_stats(mast, sigma = 3.0, iters = 5)
-daofind = DAOStarFinder(fwhm = 1.5, threshold = 5.*std)
-sources = daofind(mast-median)
+###POSSIBLE TO UNCOMMENT HERE IF YOU HAVE NO STAR LIST###
 
+#get the positions in the master, if no star list is provided 
+#mean, median, std = sigma_clipped_stats(mast, sigma = 3.0, iters = 5)
+#daofind = DAOStarFinder(fwhm = 1.5, threshold = 5.*std)
+#sources = daofind(mast-median)
 #set up for aperture photometry on the master frame
-mean, median, std = sigma_clipped_stats(mast, sigma = 3.0, iters = 5)
-positions = (sources['xcentroid'], sources['ycentroid'])
+#mean, median, std = sigma_clipped_stats(mast, sigma = 3.0, iters = 5)
+#positions = (sources['xcentroid'], sources['ycentroid'])
+#x = sources['xcentroid']
+#y = sources['ycentroid']
+
+###END POSSIBLE UNCOMMENT###
+
+###POSSIBLE TO UNCOMMENT HERE IF YOU HAVE A STAR LIST###
+#get the positions from the star list if one is provided ###COMMENT HERE###
+ticid, tmag, ra, dec = numpy.loadtxt('ffi_test.csv', unpack = 1, delimiter = ',', skiprows =1)
+
+#convert the ra/dec into positions
+os.chdir(rawdir) #changes to the raw image direcotory
+files = [f for f in glob.glob("*.fits") if isfile(join(rawdir, f))] #gets the relevant files with the proper extension
+files.sort()
+os.chdir(cdedir) #changes back to the code directory
+
+#get wcs from first calibrated file
+w = WCS(clndir+files[0])
+x, y = w.all_world2pix(ra, dec, 0)
+positions = (x, y)
+###END POSSIBLE UNCOMMENT###
 
 print 'Getting the photometry on the master frame.'
+#apertures to test for optimal apeture size
+rads = numpy.arange(2,5,.25) 
+
+#do the aperture photometry and find the optimal aperture
+apertures = [CircularAperture(positions, r=r) for r in rads]
+phot_table = aperture_photometry(mast, apertures, method = 'exact')
+idx = 0
+
+offset = numpy.zeros((len(rads),len(x)))
+for ii in range(0, len(x)):
+	if (x[ii] > 0) and (x[ii] < 2048) and (y[ii] > 0) and (y[ii] < 2048):
+		dist = numpy.sqrt((x[ii]-x)**2+(y[ii]-y)**2)
+		chk = numpy.where(dist < 6.)
+		if (len(chk[0]) == 1):
+			for jj in range(1, len(rads)):
+				mg1 = 25.-2.5*numpy.log10(phot_table[ii][jj+3])
+				mg0 = 25.-2.5*numpy.log10(phot_table[ii][jj+2])
+				offset[jj,ii] = mg1-mg0
+prv = 1.
+opt_rad = 10.
+for ii in range(0, len(rads)):
+	chk = numpy.median(offset[ii,:])	
+	if (numpy.abs(chk-prv) <= 0.001) and (rads[ii] < opt_rad):	
+		opt_rad = rads[ii]
+		print 'The optimal aperture size is '+str(opt_rad)+'.'
+	if (numpy.abs(chk-prv) > 0.001):
+		prv = chk
 
 #do the aperture photometry
-apertures = CircularAperture(positions, r = rad)
+apertures = CircularAperture(positions, r = opt_rad)
 phot_table = aperture_photometry(mast, apertures, method = 'exact')
 
-#set up for the background annuli
-annulus_apertures = CircularAnnulus(positions, r_in = rad+2, r_out = rad+4)
-bkgf_table = aperture_photometry(mast, annulus_apertures, method = 'exact')
-bkg_mean = bkgf_table['aperture_sum']/annulus_apertures.area()
+#get the background of the image
+cimg, clow, chigh = scipy.stats.sigmaclip(mast, low=2.5, high = 2.5) #do a 2.5 sigma clipping
+bkg_mean = numpy.median(cimg) #determine the sky value
+sig = numpy.std(cimg) #determine the sigma(sky)
 
 #convert to magnitudes
-flx = phot_table['aperture_sum']-(bkg_mean*apertures.area())
+flx = phot_table['aperture_sum']-(bkg_mean*(numpy.pi*opt_rad**2))
 flx_er = numpy.sqrt(phot_table['aperture_sum'])
-x_pix = sources['xcentroid']
-y_pix = sources['ycentroid']
+x_pix = x
+y_pix = y
 
 #create the magnitudes from the flux
-mag = 25.0-2.5*numpy.log10(flx)+2.5*numpy.log10(exp_time)
+mag = 25.0-2.5*numpy.log10(flx)
 err = (2.5/numpy.log(10.))*(flx_er/flx)
 
 #write the magnitudes to a file
-output = open(caldir+'master.ap', 'w')
+output = open(caldir+camera+'_'+ccd+'_master.ap', 'w')
 for ii in range(0, len(phot_table['id'])):
-	output.write(str(ii)+','+str(x_pix[ii])+','+str(y_pix[ii])+','+str(mag[ii])+','+str(err[ii])+'\n')
+	if (x_pix[ii] > 0) and (x_pix[ii] < 2048) and (y_pix[ii] > 0) and (y_pix[ii] < 2048) and (numpy.isnan(mag[ii]) != 1):
+		output.write(str(long(ticid[ii]))+','+str(x_pix[ii])+','+str(y_pix[ii])+','+str(tmag[ii])+','+str(mag[ii])+','+str(err[ii])+'\n')
 output.close()
 
 #write the fluxes to a file
-output = open(caldir+'master.flux', 'w')
+output = open(caldir+camera+'_'+ccd+'_master.flux', 'w')
 for ii in range(0, len(phot_table['id'])):
-	output.write(str(ii)+','+str(x_pix[ii])+','+str(y_pix[ii])+','+str(flx[ii])+','+str(flx_er[ii])+'\n')
+	if (x_pix[ii] > 0) and (x_pix[ii] < 2048) and (y_pix[ii] > 0) and (y_pix[ii] < 2048) and (numpy.isnan(mag[ii]) != 1):
+		output.write(str(long(ticid[ii]))+','+str(x_pix[ii])+','+str(y_pix[ii])+','+str(flx[ii])+','+str(flx_er[ii])+'\n')
 output.close()
 
 #write the star list to a file
-output = open(caldir+'starlist.txt', 'w')
+output = open(caldir+camera+'_'+ccd+'_starlist.txt', 'w')
 for ii in range(0, len(phot_table['id'])):
-	output.write(str(ii)+','+str(long(x_pix[ii]))+','+str(long(y_pix[ii]))+'\n')
+	if (x_pix[ii] > 0) and (x_pix[ii] < 2048) and (y_pix[ii] > 0) and (y_pix[ii] < 2048) and (numpy.isnan(mag[ii]) != 1):
+		output.write(str(long(ticid[ii]))+','+str(long(x_pix[ii]))+','+str(long(y_pix[ii]))+'\n')
 output.close()
