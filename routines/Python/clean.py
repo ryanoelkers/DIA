4f8dea880ae60ac2d1ca2fb5af2d433432b74ed4
ryanoelkers <ryanjamesoelkers@gmail.com>
2018-03-06 11:53:47 -0600

    Update clean.py

--- routines/Python/clean.py
+++ routines/Python/clean.py
@@ -10,12 +10,15 @@ import scipy
 from scipy import stats
 import scipy.ndimage as ndimage
 import astropy
+from astropy.io import fits
+from astropy.nddata.utils import Cutout2D
+from astropy.wcs import WCS
 import math
 import time
-import imreg_dft as ird
 
-#for reading in fits files
-from astropy.io import fits
+#libraries for image registration
+import FITS_tools
+from FITS_tools.hcongrid import hcongrid
 
 #import relevant libraries for a list
 import glob, os
@@ -27,18 +30,19 @@ from scipy.interpolate import Rbf
 
 #####UPDATE INFORMATION HERE####
 #DO YOU WANT TO FLAT FIELD AND BIAS SUBTRACT?
-biassub = 1 # yes = 1 no = 0 to bias subtract
-flatdiv = 1 # yes = 1 no = 0 to flat field
+biassub = 0 # yes = 1 no = 0 to bias subtract
+flatdiv = 0 # yes = 1 no = 0 to flat field
+align = 1# yes = 1 no = 0 to align based on coordinates
 
 #useful directories
-rawdir = '../raw/' # directory with the raw images
-cdedir = '../Python/' # code directory
-caldir = '../calib/' # directory with the calibration images such as bias & flat
-clndir = '../clean/'# directory for the cleaned images
+rawdir = '.../cal/' #directory with the raw images
+cdedir = '.../code/clean/' #directory where the code 'lives'
+caldir = 'N/A' #directory with the calibration images such as bias & flat
+clndir = '../clean/'#directory for the cleaned images to be output
 
 #sample every how many pixels? usually 32x32 is OK but it can be larger or smaller
-pix = 128 # UPDATE HERE FOR BACKGROUND SPACING
-axs = 4096 # UPDATE HERE FOR IMAGE AXIS SIZE
+pix = 32 # UPDATE HERE FOR BACKGROUND SPACING
+axs = 2048 # UPDATE HERE FOR IMAGE AXIS SIZE
 ###END UPDATE INFORMATION###
 
 #get the image list and the number of files which need reduction
@@ -47,6 +51,13 @@ files = [f for f in glob.glob("*.fits") if isfile(join(rawdir, f))] #gets the re
 files.sort()
 nfiles = len(files)
 os.chdir(cdedir) #changes back to the code directory
+
+#get the zeroth image for registration
+#read in the image
+ref, rhead = fits.getdata(rawdir+files[0], header = True)
+rhead['CRPIX1'] = 1001.
+rhead['NAXIS1'] = 2048
+rhead['NAXIS2'] = 2048
 
 #sample every how many pixels?
 bxs = 512 #how big do you want to make the boxes for each image?
@@ -65,11 +76,20 @@ if (biassub == 1):
 	bheader = blist[0].header #get the header info
 	bias = blist[0].data #get the image info
 
-
 #begin cleaning
 for ii in range(0, nfiles):
 	hld = files[ii].split('.')
-	finnme = hld[0]+'_sfb.'+hld[1]
+
+	#update the name to be appropriate for what was done to the file
+	if (biassub == 0) and (flatdiv == 0) and (align == 0): 
+		finnme = hld[0]+'_s.fits'
+	if (biassub == 1) and (flatdiv == 1) and (align == 0):
+		finnme = hld[0]+'_sfb.fits'
+	if (biassub == 0) and (flatdiv == 0) and (align == 1):
+		finnme = hld[0]+'_sa.fits'
+	if (biassub == 1) and (flatdiv == 1) and (align == 1):
+		finnme = hld[0]+'_sfba.fits'
+
 	#only create the files that don't exist
 	if (os.path.isfile(clndir+finnme) == 0):
     		#start the watch
@@ -78,9 +98,17 @@ for ii in range(0, nfiles):
 		print 'Now cleaning '+files[ii]+' at '+sts+'.'
 
 		#read in the image
-		hdulist = fits.open(rawdir+files[ii])
-		header = hdulist[0].header #get the header info
-		bigimg = hdulist[0].data #get the image info
+		orgimg, header = fits.getdata(rawdir+files[ii], header = True)
+		w = WCS(header)
+		cut = Cutout2D(orgimg, (1068,1024), (axs, axs), wcs = w)
+		bigimg = cut.data
+
+		#update the header
+		header['CRPIX1'] = 1001.
+		header['NAXIS1'] = 2048
+		header['NAXIS2'] = 2048
+
+		#get the holders ready
 		res = numpy.zeros(shape=(axs, axs)) #holder for the background 'image'
 		bck = numpy.zeros(shape=((axs/bxs)**2)) #get the holder for the image backgroudn
 		sbk = numpy.zeros(shape=((axs/bxs)**2)) #get the holder for the sigma of the image background
@@ -92,14 +120,13 @@ for ii in range(0, nfiles):
 		tts = 0
 		for oo in range(0, axs, bxs):
 			for ee in range(0, axs, bxs):
-				#print 'Splitting the image into '+str(bxs)+'x'+str(bxs)+' sub-image for x = '+str(oo)+'-'+str(oo+bxs)+' y = '+str(ee)+'-'+str(ee+bxs)+'.'
 				img = bigimg[ee:ee+bxs, oo:oo+bxs] #split the image into small subsections
-
+				
 				#calculate the sky statistics
 				cimg, clow, chigh = scipy.stats.sigmaclip(img, low=2.5, high = 2.5) #do a 2.5 sigma clipping
 				sky = numpy.median(cimg) #determine the sky value
 				sig = numpy.std(cimg) #determine the sigma(sky)
-				#print 'The median background counts of the entire image is '+str(long(sky))+' ADU/pix with a std. dev. of '+str(long(sig))+'.'
+
 				bck[tts] = sky #insert the image median background
 				sbk[tts] = sig #insert the image sigma background
 
@@ -200,31 +227,38 @@ for ii in range(0, nfiles):
 		sub = bigimg-res
 		sub = sub + mbck
 
-        	#now align the image based on the first frame in the sequence
-        	if (ii > 0):
-            		result = ird.similarity(mast, bigimg, numiter=3)
-            		bigimg = result['timg']
-		else:
-			if (ii == 0):
-				mast = bigimg
+		#align the image
+		algn = hcongrid(sub, header, rhead)
 
 		#update the header
-		prihdr = hdulist[0].header
-		prihdr.set('medback', str(mbck))
-		prihdr.set('sigback', str(sbck))
-		prihdr.set('bksub', 'yes')
+		header['CTYPE1'] = rhead['CTYPE1']
+		header['CTYPE2'] = rhead['CTYPE2']
+		header['CRVAL1'] = rhead['CRVAL1']
+		header['CRVAL2'] = rhead['CRVAL2']
+		header['CRPIX1'] = rhead['CRPIX1']
+		header['CRPIX2'] = rhead['CRPIX2']
+		header['CD1_1'] = rhead['CD1_1']
+		header['CD1_2'] = rhead['CD1_2']
+		header['CD2_1'] = rhead['CD2_1']
+		header['CD2_2'] = rhead['CD2_2']
+
+		#update the header
+		header['medback'] = mbck
+		header['sigback'] = sbck
+		header['bksub'] = 'yes'
 		if (biassub == 1):
-			prihdr.set('bias', 'yes')
+			header['bias'] = 'yes'
 		if (flatdiv == 1):
-			prihdr.set('flat', 'yes')
+			header['flat'] = 'yes'
+		if (align == 1):
+			header['align'] = 'yes'
 
 		#write out the subtraction
-		shd = fits.PrimaryHDU(sub, header=prihdr)
-		shd.writeto(clndir+finnme)
-
-		#close the current image
-		hdulist.close()
+		shd = fits.PrimaryHDU(algn, header=header)
+		shd.writeto(clndir+finnme, overwrite = True)
     	
 		#stop the watch
     		fn = time.time()
     		print 'Background subtraction for '+files[ii]+' finished in '+str(fn-st)+'s.'
+
+print 'All done! See ya later alliagtor.'
