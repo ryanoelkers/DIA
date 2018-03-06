1f2cee1506efbb59d9563cc2528c93c30ccf57c8
ryanoelkers <ryanjamesoelkers@gmail.com>
2018-03-06 11:54:20 -0600

    Update mkraw.py

--- routines/Python/mkraw.py
+++ routines/Python/mkraw.py
@@ -1,6 +1,6 @@
 #this program will make the raw magnitude data files
 
-#if you use this code, please cite Oelkers et al. 2015, AJ, 149, 50
+#if you use this code, please cite Oelkers & Stassun 2018
 #import the relevant libraries for basic tools
 import numpy
 
@@ -8,14 +8,19 @@ import numpy
 import glob, os
 from os import listdir
 from os.path import isfile, join, exists
+from time import strftime
 
 ####UPDATE INFORMATION HERE####
 ###be sure to update the number of light curves on line 40, now for simplicity, it is set to 100
 #useful directories
-cdedir = '../IDL/' # code directory
-difdir = '../diff/' # directory to put the differenced images
-caldir = '../calib/' # directory with master frame information
-lcdir  = '../lightcurve/' # directory to put the light curves
+cdedir = '../code/phot/' # code directory
+difdir = '../test/dif/' # directory to put the differenced images
+caldir = '../code/master/fin/' # directory with master frame information
+lcdir  = '../lc/' # directory to put the light curves
+
+camera = '2'
+ccd = '2'
+####END UPDATE INFORMATION####
 
 #get the flux lists
 os.chdir(difdir) #changes to the raw image direcotory
@@ -24,37 +29,45 @@ files.sort()
 nfiles = len(files)
 os.chdir(cdedir) #changes back to the code directory
 
-#read in the master star list
-ids, x, y = numpy.loadtxt(caldir+'starlist.txt', unpack = 1, delimiter = ',')
+#read in the master frame information to get the star count
+ids, xm, ym, ttmg, mstmg, mstmge = numpy.loadtxt(caldir+camera+'_'+ccd+'_master.ap', unpack =1, usecols=(0,1,2,3,4,5))
+nstars = len(ids)
 
 jd = numpy.zeros(nfiles)
-mg = numpy.zeros((nfiles,len(ids)))
-mge = numpy.zeros((nfiles,len(ids)))
+mg = numpy.zeros((nfiles, nstars))-99.00
+er = numpy.zeros((nfiles, nstars))-99.00
 
-#go through all the files
-for ii in range (0, nfiles):
-	id1, xx, yy, jds, mags, mages = numpy.loadtxt(difdir+files[ii], unpack = 1, delimiter = ',')
-	jd[ii] = jds[0]
-	mg[ii,:] = mags
-	mge[ii,:] = mages
+print 'Splitting photometry files at '+strftime("%a, %d %b %Y %H:%M:%S")+'.'
+#loop through each file to combine the files
+for ii in range(0, 10):#nfiles):
+	
+	#read in the frame file
+	idd, mx, my, jdh, mgh, mgeh = numpy.loadtxt(difdir+files[ii], unpack =1, usecols=(0,1,2,3,4,5),dtype = 'string')
 
-#openw new files
-for ii in range(0, 100):
-	if (ii < 10):
-		nm = '0000'+str(ii)+'.lc'
-	if (ii < 100) and (ii >= 10): 
-		nm = '000'+str(ii)+'.lc'
-	if (ii < 1000) and (ii >= 100):
-		nm = '00'+str(ii)+'.lc'
-	if (ii < 10000) and (ii >= 1000): 
-		nm = '00'+str(ii)+'.lc'
-	if (ii < 100000) and (ii >= 10000): 
-		nm = '0'+str(ii)+'.lc'
-	if (ii >= 100000):
-		nm = str(ii)+'.lc'
+	#put the data in the holder vectors
+	jd[ii] = jdh[0]
+	mg[ii, :] = mgh
+	#guard against bad magnitude errors
+	bd = numpy.where(mgeh == '********')
+	if (len(bd) > 0):
+		mgeh[bd]='-9.999999'
+	er[ii, :] = mgeh
 
-	outdir = open(lcdir+nm, 'w')
-	for jj in range(0, len(jd)):
-		line = str(jd[jj])+','+str(mg[jj,ii])+','+str(mge[jj,ii])+'\n'
-		outdir.write(line)
-	outdir.close()
+
+#loop through the stars make the light curves right now it is set to 100 to simiplify the code
+output2=open(lcdir+'starinfo.txtxx', 'w')
+for ii in range(0, 10):#nstars): 
+	#make a file name from the ticid
+	nm=str(long(ids[ii]))+'.lcxx'
+
+	#write out the light curve
+	output1=open(lcdir+nm, 'w')
+	for jj in range(0, nfiles):
+		 output1.write('%f %f %f\n' % (numpy.round(jd[jj], decimals = 6), numpy.round(mg[jj,ii], decimals = 6), numpy.round(er[jj,ii], decimals = 6)))
+	output1.close()
+	
+	#write out the star information for easy access
+	output2.write('%s %f %f %f %f\n' %(nm, numpy.round(xm[ii], decimals = 2), numpy.round(ym[ii], decimals = 2), numpy.round(mstmg[ii], decimals = 6), numpy.round(mstmge[ii], decimals = 6)))
+
+output2.close()
+print 'All done at '+strftime("%a, %d %b %Y %H:%M:%S")+'. See ya later alligator.'
