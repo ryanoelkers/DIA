24b1346b8e43dbe2ccaf99d454a5cb80a7ab6b7a
ryanoelkers <ryanjamesoelkers@gmail.com>
2018-03-06 11:54:04 -0600

    Update mk_master.py

--- routines/Python/mk_master.py
+++ routines/Python/mk_master.py
@@ -1,6 +1,6 @@
 #this program will combine images to make a master frame
 
-#if you use this code, please cite Oelkers et al. 2015, AJ, 149, 50
+#if you use this code, please cite Oelkers & Stassun 2018
 
 #import the relevant libraries for basic tools
 import pyfits
@@ -17,42 +17,68 @@ from os import listdir
 from os.path import isfile, join, exists
 
 ###UPDATE HERE#####
+#what field are you looking at?
+camera = '2'
+ccd = '2'
+blknum = 50 #how many images go into each holder?
+
+#this program will select the images to make the E and W master frames
 #useful directories
-cdedir = '../Python/' #code directory
-caldir = '../calib/' # directory to output the master frame
-clndir = '../clean/'# directory for the cleaned images
+cdedir = '/home/oelkerrj/FFI/code/master/' #code directory
+clndir = '/net/jovan/export/jovan/oelkerrj/FFI/test/clean/' #directory where the cleaned images reside
+
 ###END UPDATE###
 
 #get the image list and the number of files which need reduction
-os.chdir(clndir) #changes to the raw image direcotory
-files = [f for f in glob.glob("*.fits") if isfile(join(clndir, f))] #gets the relevant files with the proper extension
-files.sort()
+files = np.loadtxt(cdedir+'images.dat', dtype = 'string', unpack = 1, usecols = (0))
 nfiles = len(files)
-os.chdir(cdedir) #changes back to the code directory
 
 #iterate through the files
-expt = [None]*nfiles
 cnt = 0 #counter for the number of images used
+kk = 0 #the jumper for file placement
+
 for ii in range(0,len(files)):
-        if (ii == 0):  # get size on first iteration only
+
+	if (ii == 0):  # get size on first iteration only
 		nx = pyfits.getval(clndir+files[0], 'NAXIS2')
-                ny = pyfits.getval(clndir+files[0], 'NAXIS1')
-                all_data = np.ndarray(shape=(len(files),nx,ny))
+	        ny = pyfits.getval(clndir+files[0], 'NAXIS1')
+	        all_data = np.ndarray(shape=(blknum,nx,ny))
+		expt = np.zeros(blknum)
 
 	#read in the image
 	img_data = pyfits.getdata(clndir+files[ii])
-	expt[ii] = pyfits.getval(clndir+files[ii],'EXPTIME')
-	all_data[ii] = img_data 
-	cnt = cnt+1
-	#median combine the data
-        combined_data = np.median(all_data,axis=0)
+	expt[cnt] = pyfits.getval(clndir+files[ii],'EXPOSURE')
 
-# Write data to new file    
-new_image = pyfits.PrimaryHDU(combined_data)
-new_image.header.set('NUMCOMB', cnt)
-new_image.header.set('EXPTIME', np.median(expt))
-new_image.writeto(caldir+'master.fits',clobber=True)
-print "The master frame was created using a median of "+str(cnt)+" images."
+	#add the image to the vector
+	all_data[cnt] = img_data 
+	cnt = cnt+1
+
+	if (ii % 10 == 0) and (ii > 0):
+		print 'Finished with 10 images at '+str(time.strftime("%a %d %b %Y %H:%M:%S"))+'.'
+
+	if (ii == len(files)-1) or ((ii+1) % blknum == 0):
+		
+		#median combine the data
+		combined_data = np.median(all_data,axis=0)
+
+		# Write data to new file    
+		new_image = pyfits.PrimaryHDU(combined_data)
+		new_image.header.set('NUMCOMB', cnt)
+		new_image.header.set('EXPTIME', np.median(expt))
+
+		#print the file with the appropriate counter
+		if (kk < 10):
+			new_image.writeto(cdedir+'frames/'+camera+'_'+ccd+'_master_0'+str(kk)+'.fits',clobber=True)
+		else:
+			if (kk >= 10) and (kk < 100):
+				new_image.writeto(cdedir+'frames/'+camera+'_'+ccd+'_master_'+str(kk)+'.fits',clobber=True)
+
+		print "The master frame hold was created using a median of "+str(cnt)+" images."
+		kk = kk+1
+		cnt = 0
+
+		#clear the data file
+	        all_data = np.ndarray(shape=(blknum,nx,ny))
+		expt = np.zeros(blknum)
 
 del all_data, img_data # clear up some memory
-
