ab56eb73c0347fe51471e135e31ca1e9dce6cf00
Ryan J. Oelkers <ryanjamesoelkers@gmail.com>
2018-07-12 11:10:49 -0500

    Update to refphot.py
    Major changes to the way the optimal radius was identified. This bug was causing the star list to not print appropriately, and eventually, bigdiff.py would crash.

--- routines/Python/refphot.py
+++ routines/Python/refphot.py
@@ -37,41 +37,41 @@ clndir = '../clean/'#directory with the cleaned images
 ##END UPDATE####
 
 #read in the master frame
-mast, mheader = fits.getdata(caldir+camera+'_'+ccd+'_master.fits', header = True)
+mast, mheader = fits.getdata(caldir+camera+'_'+ccd+'_master_py.fits', header = True)
 
 ###POSSIBLE TO UNCOMMENT HERE IF YOU HAVE NO STAR LIST###
 
 #get the positions in the master, if no star list is provided 
-#mean, median, std = sigma_clipped_stats(mast, sigma = 3.0, iters = 5)
-#daofind = DAOStarFinder(fwhm = 1.5, threshold = 5.*std)
-#sources = daofind(mast-median)
+mean, median, std = sigma_clipped_stats(mast, sigma = 3.0, iters = 5)
+daofind = DAOStarFinder(fwhm = 1.5, threshold = 5.*std)
+sources = daofind(mast-median)
 #set up for aperture photometry on the master frame
-#mean, median, std = sigma_clipped_stats(mast, sigma = 3.0, iters = 5)
-#positions = (sources['xcentroid'], sources['ycentroid'])
-#x = sources['xcentroid']
-#y = sources['ycentroid']
+mean, median, std = sigma_clipped_stats(mast, sigma = 3.0, iters = 5)
+positions = (sources['xcentroid'], sources['ycentroid'])
+x = sources['xcentroid']
+y = sources['ycentroid']
 
 ###END POSSIBLE UNCOMMENT###
 
 ###POSSIBLE TO UNCOMMENT HERE IF YOU HAVE A STAR LIST###
 #get the positions from the star list if one is provided ###COMMENT HERE###
-ticid, tmag, ra, dec = numpy.loadtxt('ffi_test.csv', unpack = 1, delimiter = ',', skiprows =1)
+#ticid, tmag, ra, dec = numpy.loadtxt('ffi_test.csv', unpack = 1, delimiter = ',', skiprows =1)
 
 #convert the ra/dec into positions
-os.chdir(rawdir) #changes to the raw image direcotory
-files = [f for f in glob.glob("*.fits") if isfile(join(rawdir, f))] #gets the relevant files with the proper extension
-files.sort()
-os.chdir(cdedir) #changes back to the code directory
+#os.chdir(rawdir) #changes to the raw image direcotory
+#files = [f for f in glob.glob("*.fits") if isfile(join(rawdir, f))] #gets the relevant files with the proper extension
+#files.sort()
+#os.chdir(cdedir) #changes back to the code directory
 
 #get wcs from first calibrated file
-w = WCS(clndir+files[0])
-x, y = w.all_world2pix(ra, dec, 0)
-positions = (x, y)
+#w = WCS(clndir+files[0])
+#x, y = w.all_world2pix(ra, dec, 0)
+#positions = (x, y)
 ###END POSSIBLE UNCOMMENT###
 
 print 'Getting the photometry on the master frame.'
 #apertures to test for optimal apeture size
-rads = numpy.arange(2,5,.25) 
+rads = [1,2,3,4,5,6]
 
 #do the aperture photometry and find the optimal aperture
 apertures = [CircularAperture(positions, r=r) for r in rads]
@@ -80,23 +80,34 @@ idx = 0
 
 offset = numpy.zeros((len(rads),len(x)))
 for ii in range(0, len(x)):
+	#exclude stars near the edge of the frame
 	if (x[ii] > 0) and (x[ii] < 2048) and (y[ii] > 0) and (y[ii] < 2048):
-		dist = numpy.sqrt((x[ii]-x)**2+(y[ii]-y)**2)
+		dist = numpy.sqrt((x[ii]-x)**2+(y[ii]-y)**2) #exclude 'crowded' stars
 		chk = numpy.where(dist < 6.)
 		if (len(chk[0]) == 1):
+			#subtract the next highest radius mag from the current mag
 			for jj in range(1, len(rads)):
-				mg1 = 25.-2.5*numpy.log10(phot_table[ii][jj+3])
-				mg0 = 25.-2.5*numpy.log10(phot_table[ii][jj+2])
-				offset[jj,ii] = mg1-mg0
-prv = 1.
-opt_rad = 10.
-for ii in range(0, len(rads)):
-	chk = numpy.median(offset[ii,:])	
-	if (numpy.abs(chk-prv) <= 0.001) and (rads[ii] < opt_rad):	
-		opt_rad = rads[ii]
-		print 'The optimal aperture size is '+str(opt_rad)+'.'
-	if (numpy.abs(chk-prv) > 0.001):
-		prv = chk
+				if (phot_table[ii][jj+3] > 0):
+					mg1 = 25.-2.5*numpy.log10(phot_table[ii][jj+3])
+				if (phot_table[ii][jj+2] > 0):
+					mg0 = 25.-2.5*numpy.log10(phot_table[ii][jj+2])
+				if (numpy.isfinite(mg1)) and (numpy.isfinite(mg0)) and (mg1-mg0 > 0):
+					offset[jj,ii] = mg1-mg0
+chk_off = numpy.zeros(len(rads)-1)
+chk_rad = rads[1:]
+for ii in range(1, len(rads)):
+	nzro_idx = numpy.nonzero(offset[ii,:]) #get the index of the zeros
+	chk_off[ii-1] = numpy.median(offset[ii,nzro_idx])
+fin_rads = numpy.arange(chk_rad[0],chk_rad[-1],0.1)
+fin_off = numpy.interp(fin_rads,chk_rad,chk_off)
+
+#select the smallest radius where the next highest radius has only a slight change in magnitude
+for ii in range(1, len(fin_off)):
+	diff = numpy.abs(fin_off[ii]-fin_off[ii-1])
+	if (diff < 0.001):
+		opt_rad = fin_rads[ii-1]
+		break
+print 'The optimal aperture size is '+str(opt_rad)+'.'
 
 #do the aperture photometry
 apertures = CircularAperture(positions, r = opt_rad)
@@ -118,22 +129,24 @@ mag = 25.0-2.5*numpy.log10(flx)
 err = (2.5/numpy.log(10.))*(flx_er/flx)
 
 #write the magnitudes to a file
-output = open(caldir+camera+'_'+ccd+'_master.ap', 'w')
+output = open(caldir+camera+'_'+ccd+'_master_py.ap', 'w')
 for ii in range(0, len(phot_table['id'])):
 	if (x_pix[ii] > 0) and (x_pix[ii] < 2048) and (y_pix[ii] > 0) and (y_pix[ii] < 2048) and (numpy.isnan(mag[ii]) != 1):
 		output.write(str(long(ticid[ii]))+','+str(x_pix[ii])+','+str(y_pix[ii])+','+str(tmag[ii])+','+str(mag[ii])+','+str(err[ii])+'\n')
 output.close()
 
 #write the fluxes to a file
-output = open(caldir+camera+'_'+ccd+'_master.flux', 'w')
+output = open(caldir+camera+'_'+ccd+'_master_py.flux', 'w')
 for ii in range(0, len(phot_table['id'])):
 	if (x_pix[ii] > 0) and (x_pix[ii] < 2048) and (y_pix[ii] > 0) and (y_pix[ii] < 2048) and (numpy.isnan(mag[ii]) != 1):
 		output.write(str(long(ticid[ii]))+','+str(x_pix[ii])+','+str(y_pix[ii])+','+str(flx[ii])+','+str(flx_er[ii])+'\n')
 output.close()
 
 #write the star list to a file
-output = open(caldir+camera+'_'+ccd+'_starlist.txt', 'w')
+output = open(caldir+camera+'_'+ccd+'_starlist_py.txt', 'w')
 for ii in range(0, len(phot_table['id'])):
 	if (x_pix[ii] > 0) and (x_pix[ii] < 2048) and (y_pix[ii] > 0) and (y_pix[ii] < 2048) and (numpy.isnan(mag[ii]) != 1):
 		output.write(str(long(ticid[ii]))+','+str(long(x_pix[ii]))+','+str(long(y_pix[ii]))+'\n')
 output.close()
+
+print 'All done! See ya later alligator!'
